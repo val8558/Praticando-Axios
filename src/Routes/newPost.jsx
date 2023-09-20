720d6af1e613a869c3329f17f8ee7ae7aef642b1
Valmir <111082887+val8558@users.noreply.github.com>
2023-09-20 16:36:48 -0300

    criação e estilização da página NewPost

--- src/Routes/newPost.jsx
+++ src/Routes/newPost.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
+import './newPost.css'
 
 const NewPost = () => {
     return (
         <div className='new-post'>
-            <h1>inserir novo post</h1>
+            <h2>Inserir novo post</h2>
+            <form>
+                <div className='form-control'>
+                    <label htmlFor="title">Título</label>
+                    <input 
+                    type="text"
+                    name='title'
+                    id='title'
+                    placeholder='"Digite o Titulo'/>
+                </div>
+                <div className='form-control'>
+                    <label htmlFor="body">Título</label>
+                    <textarea 
+                    name='body'
+                    id='body'
+                    placeholder='Digite o conteúdo'
+                    ></textarea>
+                </div>
+                <input type="submit" value="criar post" className='btn'/>
+            </form>
         </div>
     )
 }
